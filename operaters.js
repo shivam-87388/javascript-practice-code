885d73e00578bfd56ec4168d817cb7b76a2a00c0
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-04-03 12:32:24 +0530

    add code

---
+++ operaters.js
@@ -0,0 +1,15 @@
+/* these are called operaters : + , -, *, /
+
+what is operends 
+opereands are values whis are given like any no aur any veriable like a ,b,c */
+
+// add two numbers and print the result
+let a=24;
+let b=12;
+console.log("sum of two no :", a+b);
+
+// another method
+let c = 24;
+let d = 12;
+let sum = c+d;
+console.log(sum);
