0b2f5d5cd5a0529d056941641b8c3111f93ff2aa
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-04-03 10:30:54 +0530

    first commit

---
+++ practice.js
@@ -0,0 +1,20 @@
+//create the object called product to store info //
+const  product = {
+    name: "pen",
+    rating: 4.5,
+    catogary: "stationary item",
+    price: 400,
+    offer: 5,
+};
+
+//change the product price400 to 500//
+product.price = 500;
+
+// increase the price by 5 rs//
+product.price = product.price + 5;
+
+//print resut command//
+console.log(product);
+
+// print the product name
+console.log(product.name);
