7dad6371fe3e89f270dcef92a795617a3df83c8c
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-04-04 11:16:37 +0530

    add post and pre increment orerator

--- operaters.js
+++ operaters.js
@@ -19,20 +19,54 @@ pre decrement --a (it will first decrement the value of a by 1 and then it will 
  */
 
 // add two numbers and print the result
-let a=24;
-let b=12;
-console.log("sum of two no :", a+b);
+// let a=24;
+//let b=12;
+// console.log("sum of two no :", a+b);
 
-// another method
+/* another method
 let c = 24;
 let d = 12;
 let sum = c+d;
 console.log(sum);
 
 console.log("a=",a, "b=",b);
+
+//addation
 console.log("a+b=", a+b);
+
+//subtraction
 console.log("a-b=", a-b);
+
+//division
 console.log("a/b=", a/b);
+
+//multiplication
 console.log("a*b=", a*b);
+
+//exponent
+console.log("a**b=", a**b);
+
+// modulus
+console.log("a%b=", a%b); */
+
+//post increment in addation
+let x = 2;
+console.log("x++", x++); // first the value of x is given and then it is incremented by 1
+console.log("the value of x is", x)
+ 
+//pre increment in addation
+let y = 3;
+console.log("++y", ++y); // first the value of y is incremented by 1 and then it is given
+console.log("the value of y is", y) 
+
+//post increment in subtraction
+let a = 5;
+console.log("a--", a--); // first the value of a is given and then it is decremented by 1
+console.log("the value of a is", a) 
+
+//pre increment in subtraction
+let b = 6;
+console.log("--b", --b); // first the value of b is decremented by 1 and then it is given
+console.log("the value of b is", b) 
 
 
