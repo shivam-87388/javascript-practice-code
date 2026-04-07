4ebdea1d99878d27468cb42cacaafb4cace8fde9
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-04-07 22:37:05 +0530

    add while loop

--- loop.js
+++ loop.js
@@ -1,5 +1,18 @@
 //for loop
 //initialization means that start point, condition means that end point, increment/decrement
-for (let num = 0; num<= 10; num++){
+/*for (let num = 0; num<= 10; num++){
     console.log(num);
+}
+//or
+/*let a;
+for(a=0; a<=5; a++){
+    console.log(a);
+}*/
+// while loop
+let i=0;
+while (i<=5){
+   
+    console.log(i);
+    i++;
+   
 }
