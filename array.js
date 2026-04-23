896c37e8ff366da3b6e5fc21964ce9d71bb2824a
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-04-23 11:30:18 +0530

    add array file

---
+++ array.js
@@ -0,0 +1,18 @@
+/*deceleration of array
+[23,24,34,"Shivam"]*/
+
+// make a variable of array
+let arr = [24,45,89,"Shivam"];
+
+// print arry
+console.log(arr);
+
+// length is starting from 1
+// print the array length
+
+console.log(arr.length);
+
+// index ois starting from 0
+// print the index of array
+console.log(arr.);
+
