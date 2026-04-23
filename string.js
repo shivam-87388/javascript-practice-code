c6f0896aade8bcf193d442c8673a8bd4bdf8aebd
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-04-23 23:30:36 +0530

    print single character by index no

--- string.js
+++ string.js
@@ -23,3 +23,6 @@ console.log("full name :",firstName.concat(lastName));
 //replace any prt in string
 // variable name .replace(change jisko krna hai vo value,aur new value)
 console.log(name.replace("singh","Singh"));
+
+//print a single character by index no
+console.log(name.charAt( 2 ));
