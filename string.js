70746b3b1da45a1ceea054c079224ce9e1b437d9
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-04-23 23:18:42 +0530

    add

--- string.js
+++ string.js
@@ -1,4 +1,11 @@
 let name = "shivamsingh";
+
+//print the string length
+console.log(name.length);
+
+//print index no of string
+console.log(name[3]);
+
 // print name in uppercase
 console.log(name.toUpperCase());
 
