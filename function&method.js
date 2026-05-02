fe3f29e26337d556f89cdac9b088629644691481
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-05-02 20:23:00 +0530

    add

--- function&method.js
+++ function&method.js
@@ -1,13 +1,15 @@
 //function paramater are like local variables they are alive only on function block
-// deceleration of function
+
+/* // deceleration of function
 function sum (a,b) { // this is called paramater where the pass the anything
 console.log(a+b);
 return a+b;
 }
 //function call
 sum(4,5); // this is called aarguments
+*/
 
-//make a multiplication function 
+/* //make a multiplication function 
  function multiplication (x,y){
     return x*y;
  }
@@ -15,5 +17,5 @@ sum(4,5); // this is called aarguments
  //call multiplication function
  let multiplyResult = multiplication(3,5);
  console.log(multiplyResult);
- 
+ */
 
