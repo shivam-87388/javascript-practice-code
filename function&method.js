badec0674c4c44a2e6ccecd874b8c848b53519f5
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-05-02 20:10:04 +0530

    add

--- function&method.js
+++ function&method.js
@@ -1,8 +1,8 @@
 // deceleration of function
-function sum (a,b) { // this is called paramater
+function sum (a,b) { // this is called paramater where the pass the anything
 console.log(a+b);
 return a+b;
 }
 //function call
-sum(4,5) // this is called aarguments 
+sum(4,5) // this is called aarguments
 
