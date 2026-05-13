cff8ffe6c533a0eb4ab874569e956a919badeedb
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-05-13 20:48:38 +0530

    add

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -1,14 +1,13 @@
 const mongoose = require ("mongoose")
 
 
-const url = process.env.Mongo_URI;
-const connect_db = ()=> {
-mongoose.connect(url)
-    .then((result) => {
-        console.log("database connected");
-    }).catch((err) => {
-        console.log("database connection failed");
-    });
-};
+const connect_db = async()=> {
+     try {
+        const connection = await mongoose
+    } catch (err) {
+        console.error("DB Connection Failed:", err.message);
+        process.exit(1); // Server band kar do agar DB connect nahi hua
+    }
+};    
 
-    module.exports = connect_db;
+module.exports = connect_db;
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const connect_db = require("./config/connection");
 require('dotenv').config();
+const cors = require("cors");
+
 
 
 
@@ -9,9 +11,12 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use('/', (req, res) => {
   res.send('Hello World!')
 })
+
+connect_db();
 
 
 app.listen(port, () => {
