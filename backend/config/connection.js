e87f619fd8ee98438caa81089762632e8940ddc7
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-05-13 21:00:53 +0530

    add

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -4,10 +4,10 @@ const mongoose = require ("mongoose")
 const connect_db = async()=> {
      try {
         const connection = await mongoose.connect(process.env.Mongo_URI);
-        console.log("connect db")
+        console.log("successfull database connected");
     } catch (err) {
-        console.error("DB Connection Failed:", err.message);
-        // Server band kar do agar DB connect nahi hua
+        console.error("connection failed:", err.message);
+        
     }
 };    
 
