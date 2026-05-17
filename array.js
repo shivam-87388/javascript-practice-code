eea164b77bd427607bd2f9ac63e2da0e0dd36578
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-05-17 18:26:29 +0530

    add

--- array.js
+++ array.js
@@ -219,14 +219,13 @@ console.log(count++); */
 
 /* find the samllest no */
 const arr = [7,2,9,1,5];
-let sum = 0;
+let sum = arr[0];
 let max = 0;
 for (let i = 0; i < arr.length; i++) {
-    sum =  arr[0];
     if (sum > arr[i]) {
-      sum = arr[i];
+        sum = arr[i];
+
         
     }
-} 
+}
 console.log(sum);
-// console.log(sum);
