0c662f79b4b0a0d6b1eaf11b008c00137471547b
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-05-18 00:17:55 +0530

    add

--- array.js
+++ array.js
@@ -280,3 +280,11 @@ negativeno =  large[0]
      /* print the no form 1 to 20;
  for (let i = 0; i <20; i++) {  
  } */
+/* Create Array of Squares */
+const arr = [1,2,3,4];
+let square = [];
+
+for (let i = 0; i < arr.length; i++) {
+   square.push(arr[i]*arr[i]);
+}
+console.log(square);
